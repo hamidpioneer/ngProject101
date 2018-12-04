0c818d47f35e2c6b3ca789398d9a1eaf7e3f42ba
Hamid Pioneer <hamidpioneer@yahoo.com>
2018-12-05 02:03:05 +0600

    END 080

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -8,16 +9,22 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
 
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
-    new Recipe('Test Recipe1', 'This is test desc', '../../../assets/images/tomato.jpg'),
-    new Recipe('Test Recipe2', 'This is test desc', '../../../assets/images/tomato.jpg'),
-    new Recipe('Test Recipe3', 'This is test desc', '../../../assets/images/tomato.jpg'),
-    new Recipe('Test Recipe4', 'This is test desc', '../../../assets/images/tomato.jpg')
+    new Recipe('Test Recipe1', 'This is test1 desc', '../../../assets/images/angular-material.png'),
+    new Recipe('Test Recipe2', 'This is test2 desc', '../../../assets/images/angular-ts.png'),
+    new Recipe('Test Recipe3', 'This is test3 desc', '../../../assets/images/angular-102.png'),
+    new Recipe('Test Recipe4', 'This is test4 desc', '../../../assets/images/Angular-Error.jpg')
   ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
