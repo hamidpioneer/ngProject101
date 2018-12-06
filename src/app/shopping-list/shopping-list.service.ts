fed82202018de407cd010207dd05e82aef6878c9
Hamid Pioneer <hamidpioneer@yahoo.com>
2018-12-07 03:33:23 +0600

    END 109

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,6 +1,10 @@
+import { EventEmitter } from '@angular/core';
+
 import { Ingredient } from '../shared/ingredient.model';
 
 export class ShoppingListService {
+
+    ingredientsChanged = new EventEmitter<Ingredient[]>();
 
     private ingredients: Ingredient[] = [
         new Ingredient('apple', 5),
@@ -13,5 +17,6 @@ export class ShoppingListService {
 
     addIngredient(ingredient: Ingredient) {
         this.ingredients.push(ingredient);
+        this.ingredientsChanged.emit(this.ingredients.slice());
     }
 }
