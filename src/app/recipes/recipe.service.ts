ad11cbe4806ca241650017697b3d9d2a74fdee03
Hamid Pioneer <hamidpioneer@yahoo.com>
2018-12-07 03:49:39 +0600

    END 110

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,16 +1,31 @@
 import { EventEmitter } from '@angular/core';
 
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
 
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('Test Recipe1', 'This is test1 desc', '../../../assets/images/angular-material.png'),
-        new Recipe('Test Recipe2', 'This is test2 desc', '../../../assets/images/angular-ts.png'),
-        new Recipe('Test Recipe3', 'This is test3 desc', '../../../assets/images/angular-102.png'),
-        new Recipe('Test Recipe4', 'This is test4 desc', '../../../assets/images/Angular-Error.jpg')
+        new Recipe(
+            'Tasty Schnitzel',
+            'A super-tasty Schnitzel - just awesome!',
+            '../../../assets/images/angular-material.png',
+            [
+                new Ingredient('Meat', 1),
+                new Ingredient('French Fries', 20)
+            ]
+        ),
+        new Recipe(
+            'Big Fat Burger',
+            'What else you need to say?',
+            '../../../assets/images/angular-ts.png',
+            [
+                new Ingredient('Buns', 2),
+                new Ingredient('Meat', 1)
+            ]
+        )
     ];
 
     getRecipe() {
