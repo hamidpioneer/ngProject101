d3713fc999917b71351977ac5b1c81ce9349cc11
Hamid Pioneer <hamidpioneer@yahoo.com>
2018-12-21 02:18:50 +0600

    END 147

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 
 const appRoutes: Routes = [
     { path: '', redirectTo: '/recipes', pathMatch: 'full' },
+    { path: 'recipes/:id', component: RecipeDetailComponent },
     { path: 'recipes', component: RecipesComponent },
     { path: 'shopping-list', component: ShoppingListComponent }
 ];
