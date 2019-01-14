6603bfe82838dfecd4209ef2a9e0e107622a9c3d
Hamid Pioneer <hamidpioneer@yahoo.com>
2019-01-15 01:57:48 +0600

    bootstrap/jquery/popper.js install

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -14,6 +14,7 @@ export class RecipeEditComponent implements OnInit {
   id: number;
   editMode = false;
   recipeForm: FormGroup;
+  myImagePath: string;
 
   constructor(
     private route: ActivatedRoute, 
@@ -91,7 +92,7 @@ export class RecipeEditComponent implements OnInit {
           Validators.required,
           Validators.pattern(/^[1-9]+[0-9]*$/)
         ])
-      });
+      })
     );
   }
 
